bd690696ab0e9a1b4a884f22bbfc514606719830
mesu-ai <mesu.s.ss@gmail.com>
2021-09-26 23:53:54 +0600

    fix and final

--- src/components/addemployee/AddEmployee.js
+++ src/components/addemployee/AddEmployee.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './AddEmployee.css';
 
 const AddEmployee = (props) => {
+    const {name,image}=props.ename;
 
 
     return (
         <div className="employeeName">
             {/* hired employee photo */}
-            <img src={props.ename.image} alt="" height="30" width="50" />
+            <img src={image} alt="" height="30" width="50" />
             {/* list of hired employee name */}
-            <h3> <span > {props.ename.name} </span></h3>
+            <h3> {name} </h3>
  
         </div>
     );
